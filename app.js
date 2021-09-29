a371356f40fa9b0846e4bd0865795cbf5a506203
Patricia Nelson <urbanearthymama@gmail.com>
2021-09-29 16:40:26 -0500

    inquirer input field

---
+++ app.js
@@ -0,0 +1,22 @@
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers))
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', generatePage(name, github), err =>{
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
